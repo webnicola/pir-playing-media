9254d6d871f881e5167bd5cd3cf2c4a2820036cd
webnicola <webnicola@gmail.com>
2017-05-15 16:02:06 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var RaspiSensors = require('raspi-sensors');
+
+var PIR = new RaspiSensors.Sensor({
+        type : "PIR",
+        pin  : 0X7
+}, "pir_sensor");
+
+PIR.fetchInterval(function(err, data) {
+        if(err) {
+          console.error("An error occured!");
+          console.error(err.cause);
+                return;
+        }
+
+        // Log the values
+        // console.log(data);
+       data.value && action();
+}, 1); // Fetch data every 1 seconds
+
+var action = function() {
+  console.log("MOVE!");
+};
